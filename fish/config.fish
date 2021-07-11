318849b57cc8285fd420e5fbd4f8b503b7925fa2
fvrests <fvrests@icloud.com>
2021-07-11 16:15:56 -0500

    update fish path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+set PATH /opt/homebrew/bin $PATH
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
